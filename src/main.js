d1bb549ec584d125581aac0903c653e7ac9a2403
RuixianZhang <zhangruixian98@vip.qq.com>
2024-03-08 00:58:18 +0000

    Function Update: English Function

--- src/main.js
+++ src/main.js
@@ -6,10 +6,11 @@ Vue.config.productionTip = false
 
 //引入Element UI
 import ElementUI from "element-ui"
+import locale from 'element-ui/lib/locale/lang/en'; // 引入英文语言包
 import "element-ui/lib/theme-chalk/index.css"
 import "@/assets/element-variables.scss"
 import "@/assets/base.css"
-Vue.use(ElementUI)
+Vue.use(ElementUI, { locale }); // 使用英文语言包
 new Vue({
   router,
   render: h => h(App),
